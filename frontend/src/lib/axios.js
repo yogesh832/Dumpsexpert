fccb765d1657bf652d73d731bb390a24b58103f5
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-05-29 16:03:06 +0530

    aded some changde

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_BASE_URL;
+const apiUrl = import.meta.env.VITE_HOSTED_API_URL;
 console.log(`API Base URL: ${apiUrl}`);
 
 export const instance = axios.create({
