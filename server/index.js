5e7b6b7e5b3778a488cc95e4ab370ad9370df32c
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-05-29 16:32:42 +0530

    just cheking

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(cors({
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+//thi is test
 
 app.use(express.json());
 app.use(cookieParser());
