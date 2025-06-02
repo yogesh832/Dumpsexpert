8658e421db61f87aea4e47314bb1e8de77e5988a
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-06-02 14:51:08 +0530

    changes manners

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 
-const PublicLayout = lazy(() => import("./layout/PublicLayout.jsx"));
 const PrivateAdminLayout   = lazy(() => import("./layout/PrivateAdminLayout"));
 const PrivateStudentLayout = lazy(() => import("./layout/PrivateStudentLayout"));
 const PrivateGuestLayout   = lazy(() => import("./layout/PrivateGuestLayout"));
+const PublicLayout = lazy(() => import("./layout/PublicLayout.jsx"));
 
 const Home = lazy(() => import("./pages/Home"));
 const NotFound = lazy(() => import("./pages/NotFound"));
