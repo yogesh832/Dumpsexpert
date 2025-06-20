623130f0ca605938c8a2ced2247c22638aff0c91
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-06-20 11:48:03 +0530

    refactor: enhance user fetching logic with loading state and error handling

--- frontend/src/layout/AuthProvider.jsx
+++ frontend/src/layout/AuthProvider.jsx
@@ -1,26 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { instance } from "../lib/axios";
 import useAuthStore from "../store";
+import LoadingSpinner from "../components/ui/LoadingSpinner";
 
 const AuthProvider = ({ children }) => {
   const setUser = useAuthStore((state) => state.setUser);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await instance.get("/api/auth/me");
+        const res = await instance.get("/api/auth/me", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
         setUser(res.data.user);
       } catch (err) {
-        console.log("Not logged in or session expired");
-        console.error(err);
-        setUser(null); 
+        console.log("Session expired or not logged in",err);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchUser();
   }, [setUser]);
 
-  return children; 
-};// AuthProvider.jsx
+  if (loading) return <div className="flex items-center justify-center p-4 h-[100vh] text-center"><LoadingSpinner/></div>;
+
+  return children;
+};
 
 export default AuthProvider;
